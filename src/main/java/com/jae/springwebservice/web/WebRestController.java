6a2b06306fc1ae6fec87d76661d343ed1be98d76
Jae Choi <j.thechois@gmail.com>
2020-01-31 23:20:43 +0900

    Add Controller & DTO

--- src/main/java/com/jae/springwebservice/web/WebRestController.java
+++ src/main/java/com/jae/springwebservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package com.jae.springwebservice.web;
 
+import com.jae.springwebservice.domain.PostsDao;
+import com.jae.springwebservice.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsDao postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
--- src/test/java/com/jae/springwebservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/jae/springwebservice/domain/PostsRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class PostsRepositoryTest {
 
     @Autowired
-    PostsRepository postsRepository;
+    PostsDao postsRepository;
 
     @After
     public void cleanup() {
